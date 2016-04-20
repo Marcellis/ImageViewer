008b593e9c40ef0bb1be3f5b62be0353dd48f3dd
Marcellis <m.m.c.m.marcellis@hva.nl>
2016-04-20 11:00:11 +0200

    List added

--- app/src/main/java/com/example/marcellis/imageviewer/MainActivity.java
+++ app/src/main/java/com/example/marcellis/imageviewer/MainActivity.java
@@ -8,8 +8,26 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button imageButton;
+    ImageView imageView;
+    int imageIndex = 1;
+    final int[] imageIds = {R.drawable.image1, R.drawable.image2, R.drawable.image3};
+
+    private ListView listView;
+
+
+    private ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +35,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        imageButton = (Button) findViewById(R.id.imageButton);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        listView = (ListView) findViewById(R.id.listView);
+
+
+//Create the List of items
+
+        String[] items = getResources().getStringArray(R.array.description_text);
+
+//Create the Array Adapter, give it a layout and a list of values
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+
+//Set the newly created adapter as the adapter for the listview
+        listView.setAdapter(adapter);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageResource(imageIds[imageIndex]);
+                imageIndex++;
+
+                if (imageIndex >= imageIds.length) {
+                    imageIndex = 0;
+                }
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
